4ac21ba7202d794dda314f01221ec8d5fc6e5293
Dan Ward <dan.steve@hotmail.co.uk>
2016-10-28 12:49:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 <html>
 <title> </title>
 <body>
@@ -14,7 +15,24 @@
 	<h2>Article 1</h2>
 	<p>This is article 1</p>
 	<?php
+	
 	echo "Today is " . date("Y/m/d") . "<br>";
+	?>
+	
+	<?php
+	echo "Starting connection to datavase<br>";
+	$db = mysql_connect("silva.computing.dundee.ac.uk", "16ac3u06","cba123");
+	mysql_select_db("16ac3d06");
+	if(!$db)
+	{
+		echo mysql_error();
+	}
+	else
+	{
+		echo "Successfully connected";
+	}
+	mysql_close($db);
+	
 	?>
 	</article>
 	<article>
