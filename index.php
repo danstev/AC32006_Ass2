48c5b16b9eed00c01035f16cdf2fa26b47a31978
Dan Ward <dan.steve@hotmail.co.uk>
2016-10-28 12:33:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<!DOCTYPE html>
 <html>
 <title> </title>
 <body>
