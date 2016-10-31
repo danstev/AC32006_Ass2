8a1c841411472c0ebd20ee0c5952ffb5c25e065f
Dan Ward <dan.steve@hotmail.co.uk>
2016-10-31 14:33:36 +0000

    NOt needed

--- test/sqlite.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new SQLite3('mydb.sq3');
-$sql = "SELECT * FROM items WHERE price < 3.00";
-$result = $db->query($sql);
-while ($row = $result->fetchArray(SQLITE3_ASSOC)){
-  echo $row['name'] . ': $' . $row['price'] . '<br/>';
-}
-unset($db); 
-
