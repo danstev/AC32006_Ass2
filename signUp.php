e666478213724b33b06553f6d5351bbed4042566
IBogd <ibogdanov888@gmail.com>
2016-11-04 00:10:50 +0000

    signUp form
    example of form for a user_accounts table, next step connect to our DB,
    by creating another php

---
+++ signUp.php
@@ -0,0 +1,20 @@
+<!-- start example form for signUp page which is will be connected to user_accounts table
+-->
+
+<form action="signup.php" method="post">
+
+	Username:<br />
+	<input name="username" type="text" /><br />
+
+	Password:<br />
+	<input name="password" type="password" /><br />
+
+	Email:<br />
+	<input name="email" type="text" /><br />
+
+	Data of Birth: <br />
+	<input name="dateofbirth" type="date" /><br />
+
+	<input type="submit" />
+
+</form>
