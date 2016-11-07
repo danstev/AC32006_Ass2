40e3dbcf36b2f52ce4faf50d53e703616cb2acc6
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-07 18:37:43 +0000

    Login modular

--- LogIn.php
+++ LogIn.php
@@ -61,12 +61,7 @@
 	<h2>Log in</h2>
 	</article>
 		<h3><?php  echo $message; ?><br/></h3>
-		<form action="login.php" method ="post">
-		Username: <input type="text" name="username" value="<?php echo htmlspecialchars($username);?>"/><br/>
-		Password: <input type="password" name="password" value=""/><br/>
-		<br/>
-		<input type="submit" name="submit" value="Submit"/>
-		</form>
+		<?php include 'forms/LoginForm.php'; ?>
 	</article>
 
 
--- forms/LoginForm.php
+++ forms/LoginForm.php
@@ -1,29 +1,6 @@
-<form action="signUPpage.php" method="post">
-
-		Username:<br />
-		<input name="username" type="text" /><br />
-	
-		First name:<br />
-		<input name="firstName" type="text" /><br />
-	
-		Last name:<br />
-		<input name="lastName" type="text" /><br />
-	
-		Phone Number:<br />
-		<input name="phoneNumber" type="numeric" /><br /> 
-
-		Email:<br />
-		<input name="email" type="email" /><br />
-
-		Password:<br />
-		<input name="passwords" type="password" /><br />
-		
-		Password repeat:<br />
-		<input name="passwordRepeat" type="password" /><br />
-
-		Data of Birth: <br />
-		<input name="dateofbirth" type="date" /><br />
-
-		<input type="submit" />
-
-	</form>
+<form action="login.php" method ="post">
+		Username: <input type="text" name="username" value="<?php echo htmlspecialchars($username);?>"/><br/>
+		Password: <input type="password" name="password" value=""/><br/>
+		<br/>
+		<input type="submit" name="submit" value="Submit"/>
+</form>
