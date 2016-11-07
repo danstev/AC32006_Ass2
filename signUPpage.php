fd4d355c010dbbaa324c29258177de0fb52dfa0a
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-07 18:26:03 +0000

    veri

--- signUPpage.php
+++ signUPpage.php
@@ -13,7 +13,6 @@
 <title> </title>
 <body>
 	<h1>Home : Welcome to Scubadiver </h1>
-	<?php include 'scripts/ConnectToDB.php';?>
 
 	<article>
 	<h2>Welcome to scuba something, buy our shit</h2>
@@ -31,44 +30,30 @@
 	</p>
 	</article>
 	
-	
-
 <?php //End of real page, only goGet/Post stuff here!!
-
-	/*
 	
-function checkIfExist($username)
-{
-	$result= MYSQL_QUERY(
-		 "SELECT * FROM clients
-		 WHERE "
-		 );
-	
-}
-
-*/
-// Require the file that connect to the database.
-//  database connection in separate file
-		
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
 	include_once('scripts/ConnectToDB.php');
 	$err = 0;
 	$errCode = "";
 
-// call our data using the $_POST predefined variable
-	
-	//Sanitised input
-	
 	//Trim removes whitespaces, so no problems inserting into db
-	$username =mysql_real_escape_string(trim($_POST['username']));// mysql_real_escape_string need to test in the labs
-//	$username = trim($_POST['username']); 
+	$username =mysql_real_escape_string(trim($_POST['username']));
 	//strip tags removes any php, html css etc tags
   	$username = strip_tags($username);
 	//htmlspeicalchars escapes some characters 
  	$username = htmlspecialchars($username);
- 	
-	
+
+ 	//Check if username is in DB, add to error if no
+
+ 	$nameCheckQuery = mysql_query("Select username from Clients where username = '$userName'");
+ 	if(mysql_num_rows($nameCheckQuery) > 0)
+ 	{
+ 		$err += 1;
+ 		$errCode += "Your username is in use, please select another username<br>";;
+ 	}
+ 
 	$Fname =mysql_real_escape_string(trim($_POST['firstName']));
   	$Fname = strip_tags($Fname);
  	$Fname = htmlspecialchars($Fname);
@@ -82,9 +67,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
  	$phoneNumber = htmlspecialchars($phoneNumber);
 	//Reg exp to only allow 0-9, before allowed a-z
 	$phoneNumber = preg_replace("([^0-9])", "", $phoneNumber);
+
+	//chech if phone no is in db, add to err if yes
+
+	$phoneCheckQuery = mysql_query("Select phoneNumber from Clients where username = '$phoneNumber'");
+ 	if(mysql_num_rows($phoneCheckQuery) > 0)
+ 	{
+ 		$err += 1;
+ 		$errCode += "Your phone number is in use, please select another phone number<br>";;
+ 	}
 	
 	//Special filter for emails
 	$email =mysql_real_escape_string (filter_var($_POST['email'], FILTER_SANITIZE_EMAIL)); // mysql_real_escape_string
+
+	$emailCheckQuery = mysql_query("Select phoneNumber from Clients where username = '$email'");
+ 	if(mysql_num_rows($emailCheckQuery) > 0)
+ 	{
+ 		$err += 1;
+ 		$errCode += "Your email is in use, please select another email<br>";;
+ 	}
 	
 	//Maybe check if both passwords are the same here, then encrypt
 	if (strcmp($_POST['passwords'], $_POST['passwordRepeat']) !== 0)
@@ -104,16 +105,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	$dateofbirth = date("y-m-d", strtotime($dateofbirth));
 	//echo $dateofbirth;
 	
-	
-	//Possibly put input validation here, i.e no previous username, email, phonenumber, password requirements, 
-
-	//Check if username, email exists
-	
-	
-	
-	//  insert information  and VALUES defines the values that
-	// we are entering
-	
 	if( $err === 0)
 	{
 		$query = "INSERT INTO clients (Fname, Lname, phoneNumber, email, userName, passwords, dateofbirth)
@@ -124,6 +115,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	}
 	else
 	{
+		//Work out how to save certain form items so user doesn't have to completely redo whole form
 		echo $errCode;		
 	}
 		
