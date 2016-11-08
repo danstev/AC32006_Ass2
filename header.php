0982068ceae72657a2600c438741dbd9805e2ed4
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-08 20:14:51 +0000

    Header stuff

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<footer style="position:absolute; bottom:0; width: 100%; height: 10%; background-color: #C0FFEE;">
+<header style="position:absolute; bottom:0; width: 100%; height: 10%; background-color: #C0FFEE;">
 <div class="container"> 
  <div class="row">
  
@@ -6,76 +6,48 @@
 		if(session_id() == '')
 		{
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>';
+			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>'; //Add logo here
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><a href="Index.php">Home</a></div>';
-			echo '<div class="col-md-1"><a href="logIn.php">Login</a></div>';
-			echo '<div class="col-md-1"><a href="signUPpage.php">Register</a></div>';
-			echo '<div class="col-md-1"><a href="Products.php">Products</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Contact Us</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Contact Us</a></div>';
+			echo '<div class="col-md-6"><h1>DiveMasters</h1></div>';
 			echo '<div class="col-md-3"></div>';
 			
 		}
 		else if($_SESSION["privilege"] === "customer")
 		{
-			echo 'LINK CURRENTLY BROKEN!!!';
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>';
+			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>'; //Add logo here
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><a href="Index.php">Home</a></div>';
-			echo '<div class="col-md-1"><a href="logIn.php">Products</a></div>';
-			echo '<div class="col-md-1"><a href="signUPpage.php">Basket</a></div>';
-			echo '<div class="col-md-1"><a href="Products.php">Checkout</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Help Desk</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Log out</a></div>';
+			echo '<div class="col-md-6"><h1>DiveMasters</h1></div>';
 			echo '<div class="col-md-3"></div>';
 			
 		}
 		else if($_SESSION["privilege"] === "employee")
 		{
-			echo 'LINK CURRENTLY BROKEN!!!';
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>';
+			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>'; //Add logo here
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><a href="Index.php">Home</a></div>';
-			echo '<div class="col-md-1"><a href="logIn.php">Branch Overview</a></div>';
-			echo '<div class="col-md-1"><a href="signUPpage.php">Stock</a></div>';
-			echo '<div class="col-md-1"><a href="Products.php">Employee Page</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Order Display</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Log Out</a></div>';
+			echo '<div class="col-md-6"><h1>DiveMasters</h1></div>';
 			echo '<div class="col-md-3"></div>';
 			
 		}
 		else if($_SESSION["privilege"] === "admin")
 		{
-			echo 'LINK CURRENTLY BROKEN!!!';
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>';
+			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>'; //Add logo here
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><a href="Index.php">Home</a></div>';
-			echo '<div class="col-md-1"><a href="logIn.php">Branch Overview</a></div>';
-			echo '<div class="col-md-1"><a href="signUPpage.php">Add supplier</a></div>';
-			echo '<div class="col-md-1"><a href="Products.php">Products</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Contact Us</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Contact Us</a></div>';
+			echo '<div class="col-md-6"><h1>DiveMasters</h1></div>';
 			echo '<div class="col-md-3"></div>';
 			
 		}
 		else {
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>';
+			echo '<div class="col-md-1"><img src="Images/1.jpg" class="img-circle" width="50" height="50"></div>'; //Add logo here
 			echo '<div class="col-md-1"></div>';
-			echo '<div class="col-md-1"><a href="Index.php">Home</a></div>';
-			echo '<div class="col-md-1"><a href="logIn.php">Login</a></div>';
-			echo '<div class="col-md-1"><a href="signUPpage.php">Register</a></div>';
-			echo '<div class="col-md-1"><a href="Products.php">Products</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Company page</a></div>';
-			echo '<div class="col-md-1"><a href="ContactUs.php">Contact Us</a></div>';
+			echo '<div class="col-md-6"><h1>DiveMasters</h1></div>';
 			echo '<div class="col-md-3"></div>';
 		}
 		
 		?>
 	</div>
 </div>
-</footer>
+</header>
