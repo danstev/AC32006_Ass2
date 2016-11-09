1a318c1ffea06659aacd108c420c3703134f3054
A-Mckinlay <a.mckinlay@dundee.ac.uk>
2016-11-09 16:56:26 +0000

    Product page displays table of all products
    Can be refined by type

--- Products.php
+++ Products.php
@@ -8,15 +8,67 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" href="style.css">
+<?php include  'Scripts\ConnectToDB.php';?>
 
 <html>
+<?php include 'header.php';?>
 <title> </title>
+
 <body>
+
 	<h1>idk : Scubadiver bullshit what did we call us?</h1>
 
 	<article>
-	<h2>what</h2>
-	<p>kjhgkhjg</p>
+	<h2>Products</h2>
+
+	 //Auto Gen this using DB?
+		<form action="Products.php" method ="post">
+  		Refine By: <select name="Product Type">
+  		<option disabled selected value> Product Type </option>
+  		<option value="All">All</option>
+   		<option value="Mask">Masks</option>
+    	<option value="BCDs">BCDs</option>
+    	<option value="Dive Computer">Dive Computers</option>
+    	<option value="Fins">Fins</option>
+    	<option value="Regulator">Regulators</option>
+    	<option value="Snorkel">Snorkels</option>
+    	<option value="Wetsuit">Wetsuits</option>
+  		</select>
+  		<input type="submit" name="submit" value="Submit"/>
+		</form>
+
+
+
+	<?php //Gets and displays all products in table
+		if(isset($_POST["submit"]) && $_POST["Product_Type"] !== "All")
+		{
+			$prodType = $_POST["Product_Type"];
+			$query = "SELECT * FROM products WHERE productType = '$prodType';";
+		}
+		else
+		{
+			$query = "SELECT * FROM products;";
+		}
+		$result = mysql_query($query);
+		echo "<br><table border=\"5\" bordercolor=\"black\"
+		cellpadding=\"10\" width=\"100%\" style=\"border-collapse:
+		collapse\" align=\"center\"><tr>";
+		if($result !== false)
+		{
+			while($row = mysql_fetch_array($result)){
+				$imagePath = $row["imageLink"];
+				echo "<td>";
+				echo $row["productType"]." </td><td>".$row["productName"]."</td><td>". "<img src = '$imagePath' >"."</td><td>".$row["description"]."</td><td>£". $row["cost"];
+				echo "</td></tr>";
+			}
+		}
+		else
+		{
+			echo "Sorry No Products Found";
+		}
+ 		echo "</table>";
+?>
+	
 	</article>
 
 
