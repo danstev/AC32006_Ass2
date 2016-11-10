86c4601d82c97e91b3da3eff3400dda1135b5c36
aaronsimon2 <aaronsimon@blueyonder.co.uk>
2016-11-10 20:04:38 +0000

    Add files via upload

--- BranchOverview.php
+++ BranchOverview.php
@@ -10,57 +10,27 @@
 <link rel="stylesheet" href="style.css">
 
 <html>
-<title> </title>
+<title>Branch Overview</title>
 <body>
 
 <?php 
 include_once('scripts/ConnectToDB.php');
-$name = $_SESSION["username"];
-$query = "SELECT * FROM user_account WHERE username = '$name' ;";
-$result = mysql_query($query);
-$accountID=$result["accountID"];
-$query2 = "SELECT * FROM employees WHERE accountID = '$accountID';";
-$employee = mysql_query($query2);
-$employee_branch = $employee["branchID"];
-$query3 = "SELECT * FROM branches WHERE branchID = '$employee_branch';";
-$branch = mysql_query($query3);
-$query4 = "SELECT * FROM employee WHERE branchID ='$employee_branch';";
-$employess = mysql_query($query4);
+$name =  "dan";//$_SESSION["username"];
+$employeeIDres = mysql_query("SELECT employeeID FROM logins WHERE username = '$name'");
+$employeeID = MySQL_fetch_row($employeeIDres);
+$branchIDres=mysql_query("SELECT branchID FROM employee WHERE employeeID = '$employeeID[0]'");
+$branchID = MySQL_fetch_row($branchIDres);
+$query = mysql_query("SELECT employee.Fname, employee.Lname, employee.position, employee.email, employee.phonenumber, employee.dateofbirth, employee.branchID, employee.salary, address.street, address.housenumber, address.city, address.country, address.postcode, logins.username 
+FROM employee
+INNER JOIN address
+ON employee.addressID=address.addressID
+INNER JOIN logins ON employee.employeeID=logins.employeeID
+WHERE employee.branchID = '$branchID[0]';");
 ?>
 
 
 
 
-<?php 
-
-while($row = mysql_fetch_assoc($branch))
-{
-$BranchID= $row["branchID"];
-$floor_staff= $row["fl_staff"];
-$Managers= $row["managers"];
-}
-
-while($row = mysql_fetch_assoc($employees))
-{
-$employeeID= $row["employeeID"];
-$position= $row["position"];
-$FirstName= $row["Fname"];
-$LastName= $row["Lname"];
-$Salary= $row["salary"];
-$PhoneNumber= $row["phonenumber"];
-$Email= $row["email"];
-$DateOfBirth= $row["dateofbirth"];
-$AddressID= $row["addressID"];
-
-
-$query3 ="SELECT*FROM address WHERE addressID ='$AddressID' ;";
-$Addressemployee = mysql_query($query3);
-
-
-}
-
-
-?>
 
 
 
@@ -71,6 +41,44 @@ $Addressemployee = mysql_query($query3);
 	<h2>what</h2>
 	<p>kjhgkhjg</p>
 	</article>
+<?php 
+echo "<br><table><tr>
+		<tr>
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th>Position</th>
+		<th>Phone Number</th>
+		<th>Date of Birth</th>
+		<th>Branch ID</th>
+		<th>Salary</th>
+		<th>Street</th>
+		<th>House Number</th>
+		<th>city</th>
+		<th>country</th>
+		<th>Postcode</th>
+		</tr>";
+
+while($row = mysql_fetch_array($query))
+  {
+  echo "<tr>";
+  echo "<td>" .$row['Fname']. "</td>";
+  echo "<td>" .$row['Lname']. "</td>";
+  echo "<td>" .$row['position']. "</td>";
+  echo "<td>" .$row['phonenumber']. "</td>";
+  echo "<td>" .$row['dateofbirth']. "</td>";
+  echo "<td>" .$row['branchID']. "</td>";
+  echo "<td>" .$row['salary']. "</td>";
+  echo "<td>" .$row['street']. "</td>";
+  echo "<td>" .$row['housenumber']. "</td>";
+  echo "<td>" .$row['city']. "</td>";
+  echo "<td>" .$row['country']. "</td>";
+  echo "<td>" .$row['postcode']. "</td>";
+  echo "<td>" .$row['username']. "</td>";
+  echo "</tr>";
+  }
+  
+  echo "</table>";
+?>
 
 
 	<?php include 'footer.php';?>
