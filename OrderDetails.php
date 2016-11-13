5db4d003113a94b500b05e19eb38ef44d9472250
A-Mckinlay <a.mckinlay@dundee.ac.uk>
2016-11-13 19:39:21 +0000

    Update OrderDetails.php

--- OrderDetails.php
+++ OrderDetails.php
@@ -8,38 +8,63 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" href="style.css">
+<?php include 'Scripts/ConnectToDB.php';?>
+<?php include 'Scripts/sessionStart.php'; ?>
 
 <html>
 <title> </title>
 <body>
-	<h1>idk : Scubadiver bullshit what did we call us?</h1>
+	 <h1>idk : Scubadiver bullshit what did we call us?</h1>
 
 	<article>
-	<h2>what</h2>
+
 	<?php
-	/*if ($_SERVER['REQUEST_METHOD'] === 'GET')
-	{
-		$orderID = $_GET["id"];
-		$cusQuery = "SELECT * from orders where orderID = '$orderID';";
-		$cusResult = mysql_query($cusQuery);
-		while($cusRow = mysql_fetch_array($cusResult))
+		if ($_SERVER['REQUEST_METHOD'] === 'GET')
 		{
+			$orderID = $_GET["orderId"];
+			$cusQuery = "SELECT clientID, totalCost FROM orders WHERE orderID = '$orderID';";
+			$cusResult = mysql_query($cusQuery);
+			$cusRow = mysql_fetch_array($cusResult);
+				
 			if($cusRow["clientID"] === $_SESSION["cusID"])
 			{
-				//Display order
+				$query = "SELECT quantity, itemCost, productID FROM items_ordered WHERE orderID = $orderID;";
+				$result = mysql_query($query);
+				echo "<br><div class=\"table-responsive\"><table border=\"5\" bordercolor=\"black\"
+				cellpadding=\"10\" width=\"100%\" style=\"border-collapse:
+				collapse\" align=\"center\" class=\"table\">
+				<tr><th>Product Image</th><th>Product Name</th><th>Quantity</th><th>Cost</th>";
+				while($row = mysql_fetch_array($result))
+				{
+					$productID = $row["productID"];
+					$query = "SELECT productName, imageLink FROM products WHERE productID = $productID;";
+					$products = mysql_query($query);
+					$products = mysql_fetch_array($products);
+					$productName = $products["productName"];
+					$imagePath = $products["imageLink"];
+					echo "<tr>";
+					echo "<td><img src = '$imagePath' class=\"img-responsive\" ></td><td>" . $productName ."</td><td>". $row["quantity"]."</td><td>£". $row["itemCost"] . "</td></td>";
+					echo "</tr>";
+				}
+				echo "<tr><td></td><td></td><td>Total Cost:</td><td>" . $cusRow["totalCost"] . "</td></tr>";
+				echo "</table></div>";
+
+				/*echo "<br><div class=\"table-responsive\"><table border=\"5\" bordercolor=\"black\"
+				cellpadding=\"10\" width=\"100%\" style=\"border-collapse:
+				collapse\" align=\"center\" class=\"table\">";*/
+
 			}
 			else
 			{
 				echo "You do not have permission to look at this.";
 			}
 		}
-	}
-	else
-	{
-		//Display form
-	}*/
-		
-		?>
+		else
+		{
+			//Display form
+		}	
+?>
+	
 	</article>
 
 
