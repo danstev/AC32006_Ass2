5265eaccd148ebb5ccf1b3352dac18ffba4dece1
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-13 17:33:17 +0000

    Update HelpTicketForm.php

--- forms/HelpTicketForm.php
+++ forms/HelpTicketForm.php
@@ -1,13 +1,32 @@
-<form action="signUPpage.php" method="post">
+<form action="HelpDesk.php" method="post" class="form-horizontal">
 
-		Order ID:<br />
-		<input name="orderID" type="text" /><br />
+	<fieldset>
+
+	<!-- Form Name -->
+	<legend>Complaint Form</legend>
+
+	<!-- Text input-->
+	<div class="form-group">
+		<label class="col-md-4 control-label" for="orderID">Order ID</label>  
+		<div class="col-md-4">
+			<input id="orderID" name="orderID" type="text" placeholder="placeholder" class="form-control input-md">
+  
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label class="col-md-4 control-label" for="complaint">Complaint</label>
+		<div class="col-md-4">                     
+			<textarea class="form-control" id="complaint" name="complaint">Enter complaint here!</textarea>
+		</div>
+	</div>
 	
+	<div class="form-group">
+		<label class="col-md-4 control-label" for="singlebutton">Single Button</label>
+		<div class="col-md-4">
+			<button type="submit" value="Submit" class="btn btn-primary">Submit</button>
+		</div>
+	</div>
 
-		Complaint:<br />
-		<input name="complaint" type="text" rows="5" cols="80" /><br />
-
-
-		<input type="submit" />
-
-	</form>
+	</fieldset>
+</form>
