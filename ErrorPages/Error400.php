32b4c04caf62244f5d4c1666e45942c96f75eb2b
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-13 14:26:44 +0000

    Add files via upload

---
+++ ErrorPages/Error400.php
@@ -0,0 +1,30 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="style.css">
+
+<html>
+<title> </title>
+<body>
+	<h1>Error 400 : Scubadiver bullshit what did we call us?</h1>
+	<?php include 'scripts/ConnectToDB.php';?>
+
+	<article>
+	<h2>Bad request</h2>
+	<p>Let me know if you get this error</p>
+	</article>
+
+
+	<?php include 'footer.html';?>
+
+	<?php include 'scripts/CloseConnection.php';?>	
+
+</body>
+
+</html>
