e1e6d067106dab09dc7331875a62372413b4edee
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-13 14:28:43 +0000

    Update OrderDetails.php

--- OrderDetails.php
+++ OrderDetails.php
@@ -16,7 +16,30 @@
 
 	<article>
 	<h2>what</h2>
-	<p>kjhgkhjg</p>
+	<?php
+	/*if ($_SERVER['REQUEST_METHOD'] === 'GET')
+	{
+		$orderID = $_GET["id"];
+		$cusQuery = "SELECT * from orders where orderID = '$orderID';";
+		$cusResult = mysql_query($cusQuery);
+		while($cusRow = mysql_fetch_array($cusResult))
+		{
+			if($cusRow["clientID"] === $_SESSION["cusID"])
+			{
+				//Display order
+			}
+			else
+			{
+				echo "You do not have permission to look at this.";
+			}
+		}
+	}
+	else
+	{
+		//Display form
+	}*/
+		
+		?>
 	</article>
 
 
