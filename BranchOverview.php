6398067d4c5159827683bccdb7c93577fa169b11
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-13 14:24:24 +0000

    Need to look in db to fix

--- BranchOverview.php
+++ BranchOverview.php
@@ -1,4 +1,4 @@
-
+<<<<<<< HEAD
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
@@ -14,72 +14,58 @@
 <title>Branch Overview</title>
 <body>
 
-<?php 
-include_once('scripts/ConnectToDB.php');
-$name =  "dan";//$_SESSION["username"];
-$employeeIDres = mysql_query("SELECT employeeID FROM logins WHERE username = '$name'");
-$employeeID = MySQL_fetch_row($employeeIDres);
-$branchIDres=mysql_query("SELECT branchID FROM employee WHERE employeeID = '$employeeID[0]'");
-$branchID = MySQL_fetch_row($branchIDres);
-$query = mysql_query("SELECT employee.Fname, employee.Lname, employee.position, employee.email, employee.phonenumber, employee.dateofbirth, employee.branchID, employee.salary, address.street, address.housenumber, address.city, address.country, address.postcode, logins.username 
-FROM employee
-INNER JOIN address
-ON employee.addressID=address.addressID
-INNER JOIN logins ON employee.employeeID=logins.employeeID
-WHERE employee.branchID = '$branchID[0]';");
-?>
-
-
-
-
-
-
-
-
 	<h1>idk : Scubadiver bullshit what did we call us?</h1>
 
 	<article>
 	<h2>what</h2>
-	<p>kjhgkhjg</p>
+	
+	
+	<?php
+		if(session_id() == '' || !isset($_SESSION['privilege']))
+		{
+			echo "You do not have permission to view this page.";
+		}
+		else if($_SESSION["privilege"] === "customer")
+		{
+			echo "You do not have permission to view this page.";			
+			
+		}
+		else if($_SESSION["privilege"] === "employee")
+		{
+			include 'scripts/ConnectToDB.php';
+			$id = $_SESSION["empID"];
+			$query = "SELECT branchID FROM employees WHERE employeeID = '$id';";
+			$branch = mysql_query($query); 
+			$branchID = '';
+			while($row = mysql_fetch_assoc($branch))
+			{
+				$branchID = $row["branchID"];
+			}
+			
+			$branchQ = "SELECT * FROM branches WHERE branchID = '$branchID';";
+			$branchQuery = mysql_query($branchQ);
+			while($row = mysql_fetch_assoc($branchQuery))
+			{
+				$brancName= $row["branchName"]; //Not sure what else is here?
+				
+			}
+			
+			include 'scripts/CloseConnection.php';
+		}
+		else if($_SESSION["privilege"] === "admin")
+		{
+			include 'scripts/ConnectToDB.php';
+			
+			
+			include 'scripts/CloseConnection.php';
+		}
+		else {
+			echo "You do not have permission to view this page.";			
+		}
+		
+		?>
 	</article>
-<?php 
-echo "<br><table><tr>
-		<tr>
-		<th>First Name</th>
-		<th>Last Name</th>
-		<th>Position</th>
-		<th>Phone Number</th>
-		<th>Date of Birth</th>
-		<th>Branch ID</th>
-		<th>Salary</th>
-		<th>Street</th>
-		<th>House Number</th>
-		<th>city</th>
-		<th>country</th>
-		<th>Postcode</th>
-		</tr>";
 
-while($row = mysql_fetch_array($query))
-  {
-  echo "<tr>";
-  echo "<td>" .$row['Fname']. "</td>";
-  echo "<td>" .$row['Lname']. "</td>";
-  echo "<td>" .$row['position']. "</td>";
-  echo "<td>" .$row['phonenumber']. "</td>";
-  echo "<td>" .$row['dateofbirth']. "</td>";
-  echo "<td>" .$row['branchID']. "</td>";
-  echo "<td>" .$row['salary']. "</td>";
-  echo "<td>" .$row['street']. "</td>";
-  echo "<td>" .$row['housenumber']. "</td>";
-  echo "<td>" .$row['city']. "</td>";
-  echo "<td>" .$row['country']. "</td>";
-  echo "<td>" .$row['postcode']. "</td>";
-  echo "<td>" .$row['username']. "</td>";
-  echo "</tr>";
-  }
-  
-  echo "</table>";
-?>
 
 
 	<?php include 'footer.php';?>
@@ -88,119 +74,3 @@ while($row = mysql_fetch_array($query))
 </body>
 
 </html>
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<link rel="stylesheet" href="style.css">
-
-<html>
-<title>Branch Overview</title>
-<body>
-
-<?php 
-include_once('scripts/ConnectToDB.php');
-<<<<<<< HEAD
-$name =  "scubaC";//$_SESSION["username"];
-=======
-$name =  "dan";//$_SESSION["username"];
->>>>>>> origin/master
-$employeeIDres = mysql_query("SELECT employeeID FROM logins WHERE username = '$name'");
-$employeeID = MySQL_fetch_row($employeeIDres);
-$branchIDres=mysql_query("SELECT branchID FROM employee WHERE employeeID = '$employeeID[0]'");
-$branchID = MySQL_fetch_row($branchIDres);
-<<<<<<< HEAD
-$query = mysql_query("SELECT employee.Fname, employee.Lname, employee.position, employee.email, employee.phonenumber, employee.dateofbirth, employee.branchID, employee.salary, address.street, address.housenumber, address.city, address.country, address.postcode, logins.username
-FROM employee
-INNER JOIN address
-ON employee.addressID=address.addressID
-LEFT JOIN logins ON employee.employeeID=logins.employeeID
-WHERE employee.branchID = '$branchID[0]';");
-$query2 = MySQL_fetch_row($query);
-
-=======
-$query = mysql_query("SELECT employee.Fname, employee.Lname, employee.position, employee.email, employee.phonenumber, employee.dateofbirth, employee.branchID, employee.salary, address.street, address.housenumber, address.city, address.country, address.postcode, logins.username 
-FROM employee
-INNER JOIN address
-ON employee.addressID=address.addressID
-INNER JOIN logins ON employee.employeeID=logins.employeeID
-WHERE employee.branchID = '$branchID[0]';");
->>>>>>> origin/master
-?>
-
-
-
-
-
-
-
-
-	<h1>idk : Scubadiver bullshit what did we call us?</h1>
-
-	<article>
-	<h2>what</h2>
-	<p>kjhgkhjg</p>
-	</article>
-<?php 
-echo "<br><table border=\"5\" bordercolor=\"black\"
-		cellpadding=\"10\" width=\"100%\" style=\"border-collapse:
-		collapse\" align=\"center\"><tr>
-echo "<br><table><tr>
-		<tr>
-		<th>First Name</th>
-		<th>Last Name</th>
-		<th>Position</th>
-		<th>Phone Number</th>
-		<th>Date of Birth</th>
-		<th>Branch ID</th>
-		<th>Salary</th>
-		<th>Street</th>
-		<th>House Number</th>
-		<th>city</th>
-		<th>country</th>
-		<th>Postcode</th>
-<<<<<<< HEAD
-		<th>Username</th>
-=======
->>>>>>> origin/master
-		</tr>";
-
-while($row = mysql_fetch_array($query))
-  {
-  echo "<tr>";
-  echo "<td>" .$row['Fname']. "</td>";
-  echo "<td>" .$row['Lname']. "</td>";
-  echo "<td>" .$row['position']. "</td>";
-  echo "<td>" .$row['phonenumber']. "</td>";
-  echo "<td>" .$row['dateofbirth']. "</td>";
-  echo "<td>" .$row['branchID']. "</td>";
-  echo "<td>" .$row['salary']. "</td>";
-  echo "<td>" .$row['street']. "</td>";
-  echo "<td>" .$row['housenumber']. "</td>";
-  echo "<td>" .$row['city']. "</td>";
-  echo "<td>" .$row['country']. "</td>";
-  echo "<td>" .$row['postcode']. "</td>";
-  echo "<td>" .$row['username']. "</td>";
-  echo "</tr>";
-  }
-  
-  echo "</table>";
-?>
-
-
-	<?php include 'footer.php';?>
-
-
-</body>
-
-</html>
-<<<<<<< HEAD
-=======
->>>>>>> bb1cfb2ea72dc3afa70887398d5f220ddb4750c1
->>>>>>> origin/master
