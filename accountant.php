a38028af2f15ccdb3e9969cd63d7363fde16066c
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-14 17:04:14 +0000

    Update accountant.php

--- accountant.php
+++ accountant.php
@@ -13,111 +13,63 @@
 <title> </title>
 <body>
 <?php include 'Scripts/sessionStart.php';?>
-
 <?php include 'header.php';?>
 <?php include 'databaseoutput.php';?>
 <?php include 'Scripts/ConnectToDB.php';?>
 
 
 
-	<h1>Finacial Transactions Of DiveMasters</h1>
-
-	<article>
-	<h2></h2>
-	<p></p>
-	</article>
-
-	<br>
-	<h2>Income Transactions</h2>
+<h1>Finacial Transactions Of DiveMasters</h1>
+<h2>Income Transactions</h2>
 	<?php 
-$query = "SELECT TransactionID ,Income , TypeOfTransaction , OrderID   FROM financial_transactions WHERE NOT (income <=> NULL);";
-$result = mysql_query($query);
+	$query = "SELECT TransactionID ,Income , TypeOfTransaction , OrderID   FROM financial_transactions WHERE NOT (income <=> NULL);";
+	$result = mysql_query($query);
+	printTable($result);
+	?>
 
-printTable($result);
-?>
-
-<?php 
-$query = "select sum(income)from financial_transactions;";
-$result = mysql_query($query);
-
-while($row = mysql_fetch_assoc($result)){
-
-$totalincome = $row["sum(income)"];
-}
-
-echo "<div class=text-center><h3>Total Income: $totalincome |GBP</h3></div>" 
-
-?>
-
-
-
-<br>
-<br>
-<br>
-	<h2>Outcome Transactions</h2>
 	<?php 
-$query = "SELECT TransactionID ,Outcome , TypeOfTransaction , OrderID   FROM financial_transactions WHERE NOT (outcome <=> NULL);";
-$result = mysql_query($query);
-
-printTable($result);
-?>
-
-<?php 
-$query = "select sum(outcome)from financial_transactions;";
-$result = mysql_query($query);
-
-while($row = mysql_fetch_assoc($result))
-{
-$totaloutcome = $row["sum(outcome)"];
-}
-
-echo "<div class=text-center><h3>Total Outcome: -$totaloutcome |GBP</h3></div>" 
-
-?>
+	$query = "select sum(income)from financial_transactions;";
+	$result = mysql_query($query);
+	while($row = mysql_fetch_assoc($result))
+	{
+		$totalincome = $row["sum(income)"];
+	}
+	echo "<div class=text-center><h3>Total Income: $totalincome |GBP</h3></div>" 
+	?>
 
 
 
+<br><br><br>
+<h2>Outcome Transactions</h2>
+	<?php 
+	$query = "SELECT TransactionID ,Outcome , TypeOfTransaction , OrderID   FROM financial_transactions WHERE NOT (outcome <=> NULL);";
+	$result = mysql_query($query);
+	printTable($result);
+	?>
 
-<br>
-<br>
-<br>
+	<?php 
+	$query = "select sum(outcome)from financial_transactions;";
+	$result = mysql_query($query);
+
+	while($row = mysql_fetch_assoc($result))
+	{
+		$totaloutcome = $row["sum(outcome)"];
+	}
+
+	echo "<div class=text-center><h3>Total Outcome: -$totaloutcome |GBP</h3></div>" 
+
+	?>
+<br><br><br>
 <h2>All The Employees That Earn Below The Average</h2>
 	<?php 
-$query = "SELECT EmployeeID, Fname , Lname , Position,  salary - (select avg(salary) from employee) as Difference from employee where salary < (select avg(salary) from employee);";
-$result = mysql_query($query);
-printTable($result);
-
-?>
-
+	$query = "SELECT EmployeeID, Fname , Lname , Position,  salary - (select avg(salary) from employee) as Difference from employee where salary < (select avg(salary) from employee);";
+	$result = mysql_query($query);
+	printTable($result);
+	?>
 <br>
-
 <div class=text-center>
-<p class="lead">These employees earn below the average. The financial department of the company should review each of them and consider any possible bonus or promotion in order to keep them satisfied and boost their productivity. 'Difference' represents how much below the average these employees earn.</p>
+	<p class="lead">These employees earn below the average. The financial department of the company should review each of them and consider any possible bonus or promotion in order to keep them satisfied and boost their productivity. 'Difference' represents how much below the average these employees earn.</p>
 </div>
-
-
-
-
-
-
-
-
-	
-	
-	
-	
-	
-	
-
 	<?php include 'footer.php';?>
-
-
 </body>
-
-
-
-
-
-
 </html>
-
