fc5165316814e970a091a5b8ae5412825b24275d
Dan Ward <dan.steve@hotmail.co.uk>
2016-11-14 20:19:39 +0000

    Update AddressAddForm.php

--- forms/AddressAddForm.php
+++ forms/AddressAddForm.php
@@ -1,21 +1,54 @@
-<form action="signUPpage.php" method="post">
-
-		Postcode :<br />
-		<input name="PostCode" type="text" /><br />
-	
-		Street :<br />
-		<input name="street" type="text" /><br />
-	
-		House number :<br />
-		<input name="houseNo" type="numeric" /><br /> 
+<form action="index.php"  method = "post" class="form-horizontal">
+	<div class="form-group">
+	  <label class="col-md-4 control-label" for="houseNumber">House Number</label>  
+	  <div class="col-md-4">
+	  <input id="houseNumber" name="houseNumber" type="text" placeholder="<?php echo $address[3]?>" class="form-control input-md">
 		
-		City :<br />
-		<input name="city" type="text" /><br />
+	  </div>
+	</div>
+
+	<!-- Text input-->
+	<div class="form-group">
+	  <label class="col-md-4 control-label" for="streetName">Street Name</label>  
+	  <div class="col-md-4">
+	  <input id="streetName" name="streetName" type="text" placeholder="<?php echo $address[2]?>" class="form-control input-md">
 		
-		County :<br />
-		<input name="county" type="text" /><br />
+	  </div>
+	</div>
 
+	<!-- Text input-->
+	<div class="form-group">
+	  <label class="col-md-4 control-label" for="city">City</label>  
+	  <div class="col-md-4">
+	  <input id="city" name="city" type="text" placeholder="<?php echo $address[4];?>" class="form-control input-md">
+		
+	  </div>
+	</div>
 
-		<input type="submit" />
+	<!-- Text input-->
+	<div class="form-group">
+	  <label class="col-md-4 control-label" for="country">Country</label>  
+	  <div class="col-md-4">
+	  <input id="country" name="country" type="text" placeholder="<?php echo $address[5];?>" class="form-control input-md">
+		
+	  </div>
+	</div>
+
+	<!-- Text input-->
+	<div class="form-group">
+	  <label class="col-md-4 control-label" for="postcode">Postcode</label>  
+	  <div class="col-md-4">
+	  <input id="postcode" name="postcode" type="text" placeholder="<?php echo $address[1];?>" class="form-control input-md">
+		
+	  </div>
+	</div>
+	
+	<!-- Button -->
+	<div class="form-group">
+	  <label class="col-md-4 control-label" for="submit"></label>
+	  <div class="col-md-4">
+		<button id="submit" name="submit" class="btn btn-primary">Submit</button>
+	  </div>
+	</div>
 
 	</form>
