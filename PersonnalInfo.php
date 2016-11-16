6c804aa3db50fc7b200f471f61ad8e38a6c8bf06
A-Mckinlay <a.mckinlay@dundee.ac.uk>
2016-11-16 17:16:47 +0000

    Update PersonnalInfo.php

--- PersonnalInfo.php
+++ PersonnalInfo.php
@@ -10,31 +10,39 @@
 <link rel="stylesheet" href="style.css">
 
 <html>
-<title>Personnal Information </title>
+<title>Personnal Information : DiveMaster</title>
 <body>
 <?php include 'scripts/sessionStart.php';?>
 <?php include 'header.php';?>
 <?php include 'scripts/ConnectToDB.php'; ?>
+<h1>Personnal Information : DiveMaster</h1>
 <?php
 
 if(session_id() == '' || !isset($_SESSION['privilege']))
 {
 	echo "You do not have permission to see this page. Please log in.";
 }
-else
-{
-	if($_SESSION["privilege"] === "customer") {
+else if($_SESSION["privilege"] === "customer")
+	{
 		$clientID = $_SESSION["cusID"];
 		$clientQuery = mysql_query("SELECT * from clients WHERE clientID = '$clientID'");
-		$clients = MySQL_fetch_row($clientQuery);
-		echo "<h2>Personal Information</h2";
+		$row = MySQL_fetch_assoc($clientQuery);
+		echo "<h2>Personal Information</h2>";
+		$customer = true;
+		include 'forms/AccountDetailsForm.php';
+
+	}
+	else if(isset($_SESSION['privilege']) && $_SESSION['privilege'] !== "customer")
+	{
+		$employeeID = $_SESSION["empID"];
+		$employeeQuery = mysql_query("SELECT * from employee WHERE employeeID = '$employeeID'");
+		$row = MySQL_fetch_assoc($employeeQuery);
+		echo "<h2>Personal Information</h2>";
+		$customer = false;
 		include 'forms/AccountDetailsForm.php';
 	}
-}
-else
-{
-		echo "You do not have permission to view this page.";
-}
+
+
 ?>
 				
 		
